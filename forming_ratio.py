2242292b5ffac8040259be848453630f1b015efc
pschwendy <peterschwende@gmail.com>
2022-09-16 11:35:17 -0400

    add comments

--- forming_ratio.py
+++ forming_ratio.py
@@ -5,7 +5,23 @@ import numpy as np
 import rowan
 import signac
 
-def calculate_fitting_space(host_verticies, patch_locations) -> Num:
+def calculate_fitting_space(host_verticies, patch_locations) -> float:
+    """
+    Calculates L, the fitting space between the first and fifth triangle in a hexamer.
+    Used in the L - h calculation.
+
+    Args
+    ----
+    host_verticies: 3 by 3 matrix as a list of lists
+        verticies of base triangle
+    patch_locations: 3 by 3 matrix as a list of lists
+        patch locations of base triangle
+
+    Returns
+    -------
+    L: float
+        the fitting space between the first and fifth triangle in a hexamer 
+    """
     modified_patches = np.array(patch_locations)
     for i in range(4):
         a = modified_patches[2]
@@ -15,7 +31,23 @@ def calculate_fitting_space(host_verticies, patch_locations) -> Num:
 
     return min([x for x,y,z in modified_patches]) - max([x for x,y,z in host_verticies])
 
-def total_fit(job, fitting_space, particle_id) -> Num:
+def total_fit(job, L, particle_id) -> float:
+    """
+    Calculates sum of L - h across possible rotations using a dtheta of 2pi/100
+
+    Args
+    ----
+    job: job being examined
+    L: float
+        the fitting space between the first and fifth triangle in a hexamer
+    particle_id: string
+        which guest particle is being examined (B or C)
+
+    Returns
+    -------
+    total_fit: float
+        sum of L - h across possible rotations
+    """
     guest_vertices = np.array(job.doc.guest_vertices[particle_id])
     total_fit = 0
 
@@ -23,7 +55,7 @@ def total_fit(job, fitting_space, particle_id) -> Num:
         theta = (d_theta / 100) * (2 * pi)
         q = rowan.from_axis_angle([4, 3, 0], theta)
         new_pos = rowan.rotate(q, guest_vertices)
-        total_fit += max(fitting_space - abs(new_pos[0][0] - new_pos[2][0]), 0)
+        total_fit += max(L - abs(new_pos[0][0] - new_pos[2][0]), 0)
     
     return total_fit
 
